4af8fdd86c1acb5d89663d1d9620342f1b72ab98
hezerac <swahn.stephen@gmail.com>
2017-04-24 18:47:45 -0400

    Update CrudRoute.php

--- Routes/CrudRoute.php
+++ Routes/CrudRoute.php
@@ -1,9 +1,19 @@
 <?php
 /**
- * Crud Route
+ * Crud Routes
+ *
  */
  
- class CrudRoute extends Router
- {}
+$route = /Path/Route();
+
+
+$route->get('/','/Controllers/CrudController:get');
+
+$route->post('/','/Controllers/CrudController:create');
+
+$route->put('/id','/Controllers/CrudController:update');
+
+$route->delete('/id','/Controllers/CrudController:delete');
+
  
 ?>
