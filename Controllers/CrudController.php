8fd1c6a7063442e89f02156e862315256958fcc8
hezerac <swahn.stephen@gmail.com>
2017-04-24 13:43:34 -0400

    Update CrudController.php

--- Controllers/CrudController.php
+++ Controllers/CrudController.php
@@ -5,7 +5,80 @@
  namespace Controllers;
  
  class CrudController
- {}
+ {
+    public function get()
+    {
+        try {
+            
+            $app = new Route();
+            $post = $app->response()->post();
+            //$cookie = Cookie::bake();
+            
+            $model = new /Models/CrudModel();
+            $data = $model->create($cookie, $post);
+            
+            $api = new Path/RestApi();
+            $api->setCode(200);
+            $api->render($data);
+        
+        } catch (Exception $e) {
+            
+            $api = new Path/ToThe/RestApi();
+            $api->setCode($e->getCode());
+            $api->render($e->getMessage());
+        }
+    }
  
+ }
+ 
+    public function create()
+    {
+        try {
+            
+            $app = new Route();
+            $post = $app->response()->post();
+            //$cookie = Cookie::bake();
+            
+            $model = new /Models/CrudModel();
+            $data = $model->create($cookie, $post);
+            
+            $api = new Path/RestApi();
+            $api->setCode(200);
+            $api->render($data);
+        
+        } catch (Exception $e) {
+            
+            $api = new Path/ToThe/RestApi();
+            $api->setCode($e->getCode());
+            $api->render($e->getMessage());
+        }
+    }
+ 
+ 
+    public function create()
+    {
+        try {
+            
+            $app = new Route();
+            $post = $app->response()->post();
+            //$cookie = Cookie::bake();
+            
+            $model = new /Models/CrudModel();
+            $data = $model->create($cookie, $post);
+            
+            $api = new Path/RestApi();
+            $api->setCode(200);
+            $api->render($data);
+        
+        } catch (Exception $e) {
+            
+            $api = new Path/ToThe/RestApi();
+            $api->setCode($e->getCode());
+            $api->render($e->getMessage());
+        }
+    }
+ 
+
+
  
 ?>
