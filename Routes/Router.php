e423f6460ec202aabb486031dabb54312d8d5d69
hezerac <swahn.stephen@gmail.com>
2017-04-24 20:03:56 -0400

    Update Router.php

--- Routes/Router.php
+++ Routes/Router.php
@@ -1,166 +1,88 @@
-
+<?php
+/**
+ * Router
+ *
+ */
+namespace Routes;
 
 class Router
 {
-class Route
+    private $uri = [];
+    
+    private $method = [];
+
+    public function get($uri, $method)
+    {		
+        $this->uri[] = $uri;
+
+        $this->method[] = $method;	
+    }	
+
+    public function post($uri, $method)  
+    {       
+        $this->uri[] = $uri;
+        
+        $this->method[] = $method;  
+    }   
+    
+    public function put($uri, $method)  
+    {       
+        $this->uri[] = $uri;
+        
+        $this->method[] = $method;  
+    }   
+    
+    public function delete($uri, $method)  
+    {       
+        $this->uri[] = $uri;
+        
+        $this->method[] = $method;  
+    } 
+
+    public function execute()	
+    {		
+        $destination = isset($_GET['uri']) ? '/' . $_GET['uri'] : '/';
+        
+
+
+        foreach($this->uri as $key => $value) {
+
+            $this->errorCheck($destination, $value); //WRONG! there will always be errors but for one. change this.
+            
+            $args = $this->getArgs($key);
+            
+            $parts = explode(':', $this->method[$key]);
+
+            $controller = new $parts[0]();
+
+            $method = $parts[1];
+
+            $controller->$method($args);
+        }	
+    }
+    
+    public function getArgs($key)
+    {   
+        if(!strpos($this->uri[$key], ':')) return null;
+        
+        $args = explode(':', $this->uri[$key]);
+        
+        $results = [];
+        
+        foreach($args as $arg) {
+            if($arg % 1 !== 0) {
+                $results[] = $arg;
+            }
+        }
+            
+        return $results ? extract(array_combine($results, $results)) : null;
+    }
+    
+    private function errorCheck($destination, $value)
+    {
+        if(!strstr($destination, $value)) {
+            throw Exception(__METHOD__ . '::' . $e->getMessage(), $e->getCode());
+        }
+    }
 
-
-
-{	
-
-
-          private $uri = [];    	
-
-
-
-
-
-          private $method = [];	
-
-
-
-
-
-          public function get($uri, $method)	
-
-
-          {		
-
-
-              $this->uri[] = $uri;
-
-
-
-
-
-              $this->method[] = $method;	
-
-
-          }	
-
-
-
-
-
-          public function post($uri, $method)  
-
-
-          {       
-
-
-              $this->uri[] = $uri;
-
-
-
-
-
-              $this->method[] = $method;  
-
-
-          }   
-
-
-
-
-
-          public function put($uri, $method)  
-
-
-          {       
-
-
-              $this->uri[] = $uri;
-
-
-
-
-
-              $this->method[] = $method;  
-
-
-          }   
-
-
-
-
-
-          public function delete($uri, $method)  
-
-
-          {       
-
-
-              $this->uri[] = $uri;
-
-
-
-
-
-              $this->method[] = $method;  
-
-
-          } 
-
-
-
-
-
-          public function execute()	
-
-
-          {		
-
-
-              $uri = isset($_GET['uri']) ? '/' . $_GET['uri'] : '/';		
-
-
-
-
-
-              foreach($this->uri as $key => $value) {
-
-
-
-
-
-                  if(strstr($uri, $value)) {
-
-
-
-
-
-                      $parts = explode(':', $this->method[$key]);
-
-
-
-
-
-                      $controller = new $parts[0]();
-
-
-
-
-
-                      $method = $parts[1];
-
-
-
-
-
-                      $controller->$method();
-
-
-                  }		
-
-
-              }	
-
-
-          }
-
-
-
-
-
-      }
 }
