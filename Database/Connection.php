cb6023b282dd589dcd90c3deaceb81b5f94f7712
hezerac <swahn.stephen@gmail.com>
2017-04-26 17:36:54 -0400

    Create Connection.php

---
+++ Database/Connection.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Connection
+ *
+ */
+namespace Database;
+
+class Connection
+{
+  
+    private static $pdo;
+      
+    public static function established()
+    {
+        return self::$pdo ?: self::$pdo = new PDO(
+            'mysql:host=localhost;dbname=specialdb;charset=utf8mb4',
+            'user',
+            'pass'
+        );
+    }
+      
+}
+      
+?>
