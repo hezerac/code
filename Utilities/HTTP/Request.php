bba9da85ba4e9d876a6324065bf1546fc7305064
hezerac <swahn.stephen@gmail.com>
2017-04-26 02:22:26 -0400

    Create Request.php

---
+++ Utilities/HTTP/Request.php
@@ -0,0 +1,25 @@
+<?php   
+/**
+* REST API
+*
+*/
+namespace Utilities\HTTP;
+
+class Request  
+{
+
+    public function get()
+    {}
+    
+    public function post()
+    {}
+    
+    public function put()
+    {}
+    
+    public function delete()
+    {}
+
+}
+
+?>
