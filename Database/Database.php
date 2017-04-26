153764518ea83423d1c8343629e5dd908871ef06
hezerac <swahn.stephen@gmail.com>
2017-04-26 17:33:39 -0400

    Create Database.php

---
+++ Database/Database.php
@@ -0,0 +1,42 @@
+/**
+ * Database
+ *
+ */
+namespace Database;
+        
+class Database
+{	
+    protected $db = null;
+
+    public function __construct()	
+    {		
+        $this->db = Connection::established();
+    }
+    
+    public function call($sql, array $params, $isStoredProcedure)
+    {
+        if($isStoredProcedure) {
+            $sql = 'CALL '. $sql .'('. $this->placeholder($params) .')';
+        }
+
+        $stmt = $this->db->prepare($sql);
+        
+        $this->bindAll($stmt, $params);
+        
+        $stmt->execute();
+    }	
+      
+    private function placeholder($params)	
+    {
+        return ':' . implode(', :', array_keys($params));	
+    }	
+      
+    private function bindAll($stmt, $params) 
+    {		
+        foreach($params as $key => $value) {			
+
+            $stmt->bindParam(':$key', $value);		
+        }
+    }
+
+}
