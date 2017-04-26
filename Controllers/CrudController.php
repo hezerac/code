f18699b076e9ff16f615b904a538e3345a9227f2
hezerac <swahn.stephen@gmail.com>
2017-04-25 20:46:01 -0400

    Update CrudController.php

--- Controllers/CrudController.php
+++ Controllers/CrudController.php
@@ -3,100 +3,101 @@
  * Crud Controller
  */
 namespace Controllers;
- 
+
+use \Models\CrudModel;
+use \Utilities\RestApi;
+use \Dir\Request //TODO: Make/add Request dir
+
 class CrudController
 {
-  
-    public function get()
+
+    public function getAll()
     {
         try {
-            
-            $app = new Request();
-            $get = $app->response()->get();
-         
-            //$cookie = Cookie::bake;
-            
-            $model = new /Models/CrudModel();
-            $data = $model->create($cookie, $get);
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode(200);
-            $api->render($data);
-        
+
+            //$cookie = (new Cookie)->bake();
+
+            $get = (new Request)->get();
+
+            $data = (new CrudModel)->getAll($cookie, $get);
+
+            $response = (new RestApi)->setCode(200)->render($data);
+
         } catch (Exception $e) {
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode($e->getCode());
-            $api->render($e->getMessage());
+
+            $response = (new RestApi)->setCode($e->getCode())->render($e->getMessage());
         }
     }
- 
+
+    public function getOne($id)
+    {
+        try {
+
+            //$cookie = (new Cookie)->bake();
+
+            $get = (new Request)->get();
+
+            $data = (new CrudModel)->getOne($cookie, $get, $id);
+
+            $response = (new RestApi)->setCode(200)->render($data);
+
+        } catch (Exception $e) {
+
+            $response = (new RestApi)->setCode($e->getCode())->render($e->getMessage());
+        }
+    }
+
     public function create()
     {
         try {
-            
-            $app = new Request();
-            $post = $app->response()->post();
-         
-            //$cookie = Cookie::bake();
-            
-            $model = new /Models/CrudModel();
-            $data = $model->create($cookie, $post);
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode(200);
-            $api->render($data);
-        
+
+            //$cookie = (new Cookie)->bake();
+
+            $post = (new Request)->post();
+
+            $data = (new CrudModel)->create($cookie, $post);
+
+            $response = (new RestApi)->setCode(200)->render($data);
+
         } catch (Exception $e) {
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode($e->getCode());
-            $api->render($e->getMessage());
+
+            $response = (new RestApi)->setCode($e->getCode())->render($e->getMessage());
         }
     }
- 
+
     public function update($id)
     {
         try {
-            
-            $app = new Request();
-            $put = $app->response()->put();
-         
-            //$cookie = Cookie::bake();
-            
-            $model = new /Models/CrudModel();
-            $data = $model->create($cookie, $put, $id);
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode(200);
-            $api->render($data);
-        
+
+            //$cookie = (new Cookie)->bake();
+
+            $put = (new Request)->put();
+
+            $data = (new CrudModel)->update($cookie, $put, $id);
+
+            $response = (new RestApi)->setCode(200)->render($data);
+
         } catch (Exception $e) {
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode($e->getCode());
-            $api->render($e->getMessage());
+
+            $response = (new RestApi)->setCode($e->getCode())->render($e->getMessage());
         }
     }
- 
+
     public function delete($id)
     {
         try {
-         
-            //$cookie = Cookie::bake();
-            
-            $model = new /Models/CrudModel();
-            $data = $model->delete($cookie, $id);
-            
-            $api = new /Utilities/RestApi();
-            $api->setCode(200);
-            $api->render($data);
-        
+
+            //$cookie = (new Cookie)->bake();
+
+            $delete = (new Request)->delete();
+
+            $data = (new CrudModel)->delete($cookie, $delete, $id);
+
+            $response = (new RestApi)->setCode(200)->render($data);
+
         } catch (Exception $e) {
             
-            $api = new /Utilities/RestApi();
-            $api->setCode($e->getCode());
-            $api->render($e->getMessage());
+            $response = (new RestApi)->setCode($e->getCode())->render($e->getMessage());
         }
     }
 
