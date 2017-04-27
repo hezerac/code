0eba1cb9f78c86dc3f2b6cef44e61a9657adb1c8
hezerac <swahn.stephen@gmail.com>
2017-04-26 23:24:12 -0400

    Update Request.php

--- Utilities/HTTP/Request.php
+++ Utilities/HTTP/Request.php
@@ -9,22 +9,26 @@ class Request
 {
 
     public function get()
-    {
-        //return get_headers($url);
+    {   
+        header('HTTP/1.1 200 OK');
+        return [];
     }
     
     public function post()
     {
+        header('HTTP/1.1 201 Created');
         return $_POST;
     }
     
     public function put()
     {
+        header('HTTP/1.1 201 Created');
         return $_POST;
     }
     
     public function delete()
     {
+        header('HTTP/1.1 204 No Content');
         return [];
     }
 
