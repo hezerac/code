615711bfbc1bf83dac69c9657d84130e3bc47905
hezerac <swahn.stephen@gmail.com>
2017-04-27 19:01:25 -0400

    Create header.php

---
+++ Views/Templates/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= $metas ?>
+        
+        <title><?= $title ?></title>
+        
+        <link href="/public/css/pretty.css" rel="stylesheet">
+        <?= $links ?>
+        <?= $scripts ?>
+    </head>
+    <body>
+        <header>
+            <nav><?= $navigation ?></nav>
+        </header>
