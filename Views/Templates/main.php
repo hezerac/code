d3f658ebe1ed392fe2965a8179692ba00e1773bd
hezerac <swahn.stephen@gmail.com>
2017-04-28 12:52:26 -0400

    Update main.php

--- Views/Templates/main.php
+++ Views/Templates/main.php
@@ -1,3 +1,7 @@
         <main>
             <!-- all sthis stuff -->
         </main>
+
+        <? $leftSideBar ?: '<!-- No Left Sidebar -->' ?>
+
+        <? $rightSideBar ?: '<!-- No Right Sidebar -->' ?>
