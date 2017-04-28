580544b56f986bd931a7223e3c1e85884dab500b
hezerac <swahn.stephen@gmail.com>
2017-04-28 12:35:09 -0400

    Create master-template.php

---
+++ Views/Templates/master-template.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Master Template
+ *
+ */
+
+require 'header.php';
+
+require 'main.php';
+
+require 'left-sidebar.php';
+
+require 'right-sidebar.php';
+
+require 'footer.php';
+
+?>
+
