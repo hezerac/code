0268c3883d2bae1d35988c48060c0166022773e0
hezerac <swahn.stephen@gmail.com>
2017-04-29 09:21:40 -0400

    Update Router.php

--- App/Routes/Router.php
+++ App/Routes/Router.php
@@ -1,1 +1,48 @@
 <?php
+/**
+ * Router
+ *
+ */
+namespace Routes;
+
+class Router
+{
+    private $uri = [];
+    
+    private $method = [];
+    
+    public function run()   
+    {       
+        $uri = htmlspecialchars($_GET['uri']);
+        
+        $destination = isset($uri) ? '/' . $uri : '/'; //TODO: fix this business
+
+        foreach($this->uri as $key => $value) {
+            
+            if(!strstr($destination, $value)) continue;
+            
+            $parts = explode(':', $this->method[$key]);
+
+            $controller = new $parts[0]();
+
+            $method = $parts[1];
+            
+            $args = (substr_count($value, ':') > 1)
+                ? $this->getArgs($value) 
+                : $this->getArg($value);
+
+            $controller->$method($args);
+        }   
+    }
+
+    public function get($uri, $method)
+    {       
+        $this->add($uri, $method))
+    }   
+
+    public function post($uri, $method)  
+    {       
+        $this->add($uri, $method))  
+    }   
+    
+    public function put($uri, $method)  
