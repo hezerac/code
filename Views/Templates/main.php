ac0d7f05ddca765adba4ebec8d45c005363878cc
hezerac <swahn.stephen@gmail.com>
2017-05-02 19:08:45 -0400

    Delete main.php

--- Views/Templates/main.php
+++
@@ -1,7 +0,0 @@
-        <main>
-            <!-- all sthis stuff -->
-        </main>
-
-        <? $leftSideBar ?: '<!-- No Left Sidebar -->' ?>
-
-        <? $rightSideBar ?: '<!-- No Right Sidebar -->' ?>
