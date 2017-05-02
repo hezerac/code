18fefd8892ef36aaa36a53f441415a628d005587
hezerac <swahn.stephen@gmail.com>
2017-05-02 19:04:46 -0400

    Create header.php

---
+++ App/Views/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?= $metas ?>
+        
+        <title><?= $title ?></title>
+        
+        <link href="/public/css/pretty.css" rel="stylesheet">
+        <?= $links ?>
+        
+        <script></script>
+        <?= $headScripts ?>
+    </head>
+    <body>
+        <header>
+            <nav><?= $navigation ?></nav>
+        </header>
