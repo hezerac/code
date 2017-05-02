d59d12b121d1f7a0493881f72dc24beebc5a1112
hezerac <swahn.stephen@gmail.com>
2017-05-02 19:08:19 -0400

    Create main.php

---
+++ App/Views/main.php
@@ -0,0 +1,7 @@
+        <main>
+            <!-- main content -->
+        </main>
+
+        <? $leftSideBar ?? '<!-- No Left Sidebar -->' ?>
+
+        <? $rightSideBar ?? '<!-- No Right Sidebar -->' ?>
