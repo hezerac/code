8d0c8c02f66cff638a366fc020a25ecc337d54a2
hezerac <swahn.stephen@gmail.com>
2017-05-02 19:03:14 -0400

    Create master.php

---
+++ App/Views/master.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Master Template
+ *
+ */
+    
+require 'header.php';
+    
+require 'main.php';
+    
+require 'left-sidebar.php';
+    
+require 'right-sidebar.php';
+    
+require 'footer.php';
