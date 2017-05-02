7839627d44cc07237cc9c7abe911336b06d4209d
hezerac <swahn.stephen@gmail.com>
2017-05-02 10:17:26 -0400

    Update CrudController.php

--- App/HTTP/Controllers/CrudController.php
+++ App/HTTP/Controllers/CrudController.php
@@ -16,15 +16,15 @@ class CrudController
     public function getAll()
     {
         try {
-
+            
             $user = (new Cookie)->getCookie(); //TODO: make Cookie class
-
+            
             $data = (new CrudModel)->getAll($user);
-
+            
             $response = (new Response)->setCode(200)->json($data);
-
+            
         } catch (Exception $e) {
-
+            
             $error = (new Response)->setCode($e->getCode())->json($e->getMessage());
         }
     }
@@ -32,15 +32,15 @@ class CrudController
     public function getOne($id)
     {
         try {
-
+            
             $user = (new Cookie)->getCookie();
-
+            
             $data = (new CrudModel)->getOne($user, $id);
-
+            
             $response = (new Response)->setCode(200)->json($data);
-
+            
         } catch (Exception $e) {
-
+            
             $error = (new Response)->setCode($e->getCode())->json($e->getMessage());
         }
     }
@@ -48,17 +48,17 @@ class CrudController
     public function create()
     {
         try {
-
+            
             $user = (new Cookie)->getCookie();
-
+            
             $request = (new Router)->request();
-
+            
             $data = (new CrudModel)->create($user, $request);
-
+            
             $response = (new Response)->setCode(201)->json($data);
-
+            
         } catch (Exception $e) {
-
+            
             $error = (new Response)->setCode($e->getCode())->json($e->getMessage());
         }
     }
@@ -66,17 +66,17 @@ class CrudController
     public function update($id)
     {
         try {
-
+            
             $user = (new Cookie)->getCookie();
-
+            
             $request = (new Router)->request();
-
+            
             $data = (new CrudModel)->update($user, $request, $id);
-
+            
             $response = (new Response)->setCode(201)->json($data);
-
+            
         } catch (Exception $e) {
-
+            
             $error = (new Response)->setCode($e->getCode())->json($e->getMessage());
         }
     }
@@ -84,13 +84,13 @@ class CrudController
     public function delete($id)
     {
         try {
-
+            
             $user = (new Cookie)->getCookie();
-
+            
             $data = (new CrudModel)->delete($user, $id);
-
+            
             $response = (new Response)->setCode(204)->json($data);
-
+            
         } catch (Exception $e) {
             
             $error = (new Response)->setCode($e->getCode())->json($e->getMessage());
