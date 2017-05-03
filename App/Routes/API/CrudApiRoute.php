4a789013657a0f4762de455108b3205e24bb3743
hezerac <swahn.stephen@gmail.com>
2017-05-03 12:42:25 -0400

    Update CrudApiRoute.php

--- App/Routes/API/CrudApiRoute.php
+++ App/Routes/API/CrudApiRoute.php
@@ -5,17 +5,17 @@
  */
 namespace App\Routes\API;
     
-$app->get('/','CrudController:getAll')
+$app->get('/crud/','CrudController:getAll')
     ->name('crud.get.all');
     
-$app->get('/:id','CrudController:getOne')
+$app->get('/crud/:id','CrudController:getOne')
     ->name('crud.get.one');
     
-$app->post('/','CrudController:create')
+$app->post('/crud/','CrudController:create')
     ->name('crud.create');
     
-$app->put('/:id','CrudController:update')
+$app->put('/crud/:id','CrudController:update')
     ->name('crud.update');
     
-$app->delete('/:id','CrudController:delete')
+$app->delete('/crud/:id','CrudController:delete')
     ->name('crud.delete');
