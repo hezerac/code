7a9c8af591c475f3a4c27448446de22a50d11946
hezerac <swahn.stephen@gmail.com>
2017-05-06 16:42:55 -0400

    Update crud_template.php

--- App/Views/Templates/crud_template.php
+++ App/Views/Templates/crud_template.php
@@ -9,12 +9,17 @@ $title = 'Crud Example';
     
 $mainContent = <<<HTML
     <h1>Heading</h1>
+    
     <p>Description.</p>
+    
     <form action="" method="">
+    
         <label for="">Input:</label>
-        <input type="text" id="">
+        <input type="text" name="" id="">
+        
         <label for="">Textarea:</label>
-        <textarea id=""></textarea>
+        <textarea name="" id=""></textarea>
+        
         <input type="submit" value="Submit" id="">
     </form>
 HTML;
