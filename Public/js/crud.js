2fa9de24f2d3fdaeafb25481f3ffd22a76fc0eb1
hezerac <swahn.stephen@gmail.com>
2017-05-06 22:11:06 -0400

    Update crud.js

--- Public/js/crud.js
+++ Public/js/crud.js
@@ -1,3 +1,13 @@
-{
-    //
-}
+//class Crud extends Create.Section {
+    
+    let fragment = document.createDocumentFragment();
+
+    let div = document.createElement('div');
+
+    div.innerHTML = 'text';
+
+	frag.appendChild(div);
+
+    document.body.main.appendChild(fragment);
+
+//}
