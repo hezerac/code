3428eba21b6f96161f58f6d4e282615adef110f6
hezerac <swahn.stephen@gmail.com>
2017-05-06 20:08:16 -0400

    Update crud_template.php

--- App/Views/Templates/crud_template.php
+++ App/Views/Templates/crud_template.php
@@ -7,21 +7,12 @@
 
 $title = 'Crud Example';
     
-$mainContent = <<<HTML
-    <h1>Heading</h1>
-    
-    <p>Description.</p>
-    
-    <form action="" method="">
-    
-        <label for="">Input:</label>
-        <input type="text" name="" id="">
-        
-        <label for="">Textarea:</label>
-        <textarea name="" id=""></textarea>
-        
-        <input type="submit" value="Submit" id="">
-    </form>
+$leftSidebar = <<<HTML
+    <aside>
+        <nav>
+            <a>a link emerges</a>
+        </nav>
+    </aside>
 HTML;
     
 $footerScripts = '<script src="/public/js/crud.js"></script>';
