221d538d9c8496743ea0c4f6281b896e9c897d3c
hezerac <swahn.stephen@gmail.com>
2017-05-09 18:31:35 -0400

    Update crud.js

--- Public/js/crud.js
+++ Public/js/crud.js
@@ -15,14 +15,14 @@ const createFragments = () =>
     };
 }
 
-fetch(url).then(response => response.json).then(data => 
+fetch(url).then(response => response.json()).then(data => 
 {
 	const fragment = createFragments();
-
+	
 	Object.keys(data).forEach(key => 
 		fragment.add(data[key], 'div');
 	);
-
+	
 	fragment.make();
 });
 
