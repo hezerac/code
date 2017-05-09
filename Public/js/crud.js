82325e1b59b965e868cee59c31fb6849d836483c
hezerac <swahn.stephen@gmail.com>
2017-05-09 18:17:08 -0400

    Update crud.js

--- Public/js/crud.js
+++ Public/js/crud.js
@@ -1,22 +1,29 @@
-
-const create = (data, elements = []) =>
-{
+const createFragments = () =>
+{    
     const fragment = document.createDocumentFragment();
     
-    Object.keys(data).forEach(key =>
-    {
-        const element = document.createElement(elements[key] || 'div');
-        
-        const content = document.createTextNode(data[key]);
-        
-        fragment.appendChild(element).appendChild(content);
-    });
-    
-    document.querySelector('main').appendChild(fragment);
+    return {        
+        add(content, element) {         
+            let c = document.createTextNode(content);
+            let e = document.createElement(element);
+            fragment.appendChild(e).appendChild(c);        
+        },        
+        make() {            
+            if (!fragment.hasChildNodes()) return;            
+            document.querySelector('main').appendChild(fragment);        
+        }    
+    };
 }
 
-fetch(url)
-    .then(response => response.json)
-    .then(data => create(data));
+fetch(url).then(response => response.json)
+    .then(data => {
+        const fragment = createFragments();
+    
+        Object.keys(data).forEach(key => 
+            fragment.add(data[key], 'div');
+        );
+	    
+        fragment.make();
+    });
 
 
