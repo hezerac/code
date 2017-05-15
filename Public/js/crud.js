fa9cc84ab6bfb427aa7b8a79acd3d096b0911094
hezerac <swahn.stephen@gmail.com>
2017-05-15 09:18:51 -0400

    Update crud.js

--- Public/js/crud.js
+++ Public/js/crud.js
@@ -2,13 +2,10 @@ const createEventListeners = (events = {}) =>
 {
     return {
         create(event, callback) {
-            
             events.event = callback;
         },
         render() {
-            
             for (let key in events) {
-                
                 document.body.addEventListener(key, events[key]());
             }
         }
@@ -20,28 +17,20 @@ const createFragments = () =>
     const fragment = document.createDocumentFragment();
     
     const setAttributes = (element, attributes) => {
-        
         for (let key in attributes) {
-            
             element.setAttribute(key, attributes[key]);
         }
     };
     
     return {
         create(content, element = 'div', attributes = {}) {
-            
             let c = document.createTextNode(content);
-            
             let e = document.createElement(element);
-            
             if (Object.keys(attributes).length) setAttributes(e, attributes);
-            
             fragment.appendChild(e).appendChild(c);
         },
         render(element = 'main') {
-            
             if (!fragment.hasChildNodes()) return;
-            
             document.querySelector(element).appendChild(fragment);
         }
     };
