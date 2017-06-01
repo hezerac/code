c62ca94e4c716859b18f1e8e0da28b835a2e8cf1
hezerac <swahn.stephen@gmail.com>
2017-06-01 10:04:58 -0400

    Update Sanitizer.php

--- App/Utilities/Sanitizer.php
+++ App/Utilities/Sanitizer.php
@@ -7,7 +7,7 @@ namespace App\Utilities;
     
 class Sanitizer
 {
-    public function escape(array $data)
+    public function escape(array $data) : array
     {
         return filter_var_array($data, FILTER_SANITIZE_SPECIAL_CHARS);
     }
