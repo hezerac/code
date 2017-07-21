d4d18736267b9ec8d5871b22bab96ef4f2a23709
hezerac <swahn.stephen@gmail.com>
2017-07-21 18:25:30 -0400

    Update override.php

--- App/Views/override.php
+++ App/Views/override.php
@@ -5,23 +5,16 @@
  * Initializes values which can be overridden by {$currentScript}_template.php
  *
  */
-    
-$metas = '';
-    
-$title = 'Default Title';
-    
-$links = '';
-    
-$headScripts = '';
-    
-$navigation = '';
-    
-$leftSideBar = null;
-    
-$rightSideBar = null;
-    
-$footer = '';
-    
-$footerScripts = '';
+$template = [
+    'meta' => '',
+    'title' => 'Default Title',
+    'links' => '',
+    'headScripts' => '',
+    'navigation' => '',
+    'leftSideBar' => null,
+    'rightSideBar' => null,
+    'footer' => '',
+    'footerScripts' => ''
+];
     
 require '/Templates/' . pathinfo($currentScript, PATHINFO_FILENAME) . '_template.php';
