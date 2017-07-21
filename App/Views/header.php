d18a344a62c59442342e3f0effe0f829b9c2a143
hezerac <swahn.stephen@gmail.com>
2017-07-21 18:17:40 -0400

    Update header.php

--- App/Views/header.php
+++ App/Views/header.php
@@ -3,20 +3,20 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <?= $metas ?>
+        <?= $template['metas']; ?>
         
-        <title><?= $title ?></title>
+        <title><?= $template['title']; ?></title>
         
         <link href="/public/css/pretty.css" rel="stylesheet">
-        <?= $links ?>
+        <?= $template['links']; ?>
         
         <script></script>
-        <?= $headScripts ?>
+        <?= $template['headScripts']; ?>
     </head>
     <body>
         <header>
             <a href="/">
                 <img src="images/logo.png" alt="Logo">
             </a>
-            <nav><?= $navigation ?></nav>
+            <nav><?= $template['navigation']; ?></nav>
         </header>
