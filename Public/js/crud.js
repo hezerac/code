a3fe9932dab4e554cb4550a13f604f483afdd9ac
hezerac <swahn.stephen@gmail.com>
2017-08-11 14:37:48 -0400

    Update crud.js

--- Public/js/crud.js
+++ Public/js/crud.js
@@ -9,14 +9,18 @@ const Dashboard = data => {
     
         component.addElement('article', {'id': 'parent'});
         
-        component.addElement('h1', {}, data.title).appendTo('parent');
+        component.addElement('h1', {}, data.title)
+            .appendTo('parent');
     
-        component.addElement('button', {'onclick': 'action()'}, 'Submit').appendTo('parent');
+        component.addElement('button', {'onclick': 'action()'}, 'Submit')
+            .appendTo('parent');
         
-        component.addElement('p', {}, data.text).appendTo('parent');
+        component.addElement('p', {}, data.text)
+            .appendTo('parent');
     
         Object.keys(data).forEach(key => {
-            component.addElement('p', {'class': 'examples'}, data.value[key]).appendTo('parent');
+            component.addElement('p', {'class': 'examples'}, data.value[key])
+                .appendTo('parent');
         });
     
         component.render();
