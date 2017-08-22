a73b6f1236764c3044d322835a64d41a2bd6e8b9
hezerac <swahn.stephen@gmail.com>
2017-08-22 00:09:01 -0400

    Update New.js

--- Public/js/New.js
+++ Public/js/New.js
@@ -27,7 +27,9 @@ const childComponent = props =>
         };
         //Core renders components internally
         render() {
-            return {'h1': {'class': props}};
+            return [
+                {'h1': {'class': props}}
+            ];
         }
     };
 };
