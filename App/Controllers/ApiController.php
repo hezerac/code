59b6c6918c9be2a1cdd74d5e5f0670fe921f5a3b
hezerac <swahn.stephen@gmail.com>
2017-08-25 16:29:04 -0400

    Update ApiController.php

--- App/Controllers/ApiController.php
+++ App/Controllers/ApiController.php
@@ -10,21 +10,12 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
  
 use App\Interfaces\ApiInterface;
+
+use App\Traits\ApiControllerTrait;
  
 use App\Models\Model;
  
-class ApiController implements ApiInterface
+class SomeApiController implements ApiInterface
 {
-    public function send(Request $request, Response $response)
-    {
-        try {
-        
-            $data = (new Model)->getData($request);
-        
-            return $response->withJson($data);
-            
-        } catch (\Exception $e) {
-        
-            return $response->withJson($e->getMessage(), $e->getCode());
-    }
+    use ApiControllerTrait;
 }
