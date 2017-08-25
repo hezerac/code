3fe3216df381a007cf067dd40ba8a57a446b9979
hezerac <swahn.stephen@gmail.com>
2017-08-25 16:30:11 -0400

    Update ApiController.php

--- App/Controllers/ApiController.php
+++ App/Controllers/ApiController.php
@@ -10,7 +10,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
  
 use App\Interfaces\ApiInterface;
-
+ 
 use App\Traits\ApiControllerTrait;
  
 use App\Models\Model;
