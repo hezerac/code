a84eefd653fd4990701935b8341854ac3ee11c8f
hezerac <swahn.stephen@gmail.com>
2017-08-25 16:22:53 -0400

    Create ApiController.php

---
+++ App/Controllers/ApiController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Api Controller
+ *
+ */
+namespace App\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Psr\Http\Message\ResponseInterface as Response;
+
+use App\Interfaces\ApiInterface;
+
+use App\Models\Model;
+
+class ApiController implements ApiInterface
+{
+    public function send(Request $request, Response $response)
+    {
+        try {
+        
+            $data = (new Model)->getData($request);
+        
+            return $response->withJson($data);
+            
+        } catch (\Exception $e) {
+        
+            return $response->withJson($e->getMessage(), $e->getCode());
+    }
+}
