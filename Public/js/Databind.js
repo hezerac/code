eb6794078db8421d3c0dc3c5f0be51b64f210eb0
hezerac <swahn.stephen@gmail.com>
2017-08-26 13:27:13 -0400

    Update Databind.js

--- Public/js/Databind.js
+++ Public/js/Databind.js
@@ -4,21 +4,20 @@ const scope = {};
     const attachEvent = classNames => {
         classNames.forEach(className => {
             let elements = document.getElementsByClassName(className);
-            Object.keys(elements).forEach(key => {
-                elements[key].onkeyup = () => {
-                    for (let index in elements) {
-                        elements[index].value = this.value;
-                    }
-                };
-            });
-            Object.defineProperty($scope, className, {
-                set(newValue) {
-                    for (var index in elements) {
-                        elements[index].value = newValue;
-                    }
-                }
-            });
+            createAciom(elements);
+            createProps(elements);
         });
     };
+    const createAction = elements => Object.keys(elements).forEach(key => {
+        elements[key].onkeyup = () => {
+            setValues(elements, this.value);
+        };
+    });
+    const createProps = elements => Object.defineProperty(scope, className, {
+        set(newValue) {
+            setValues(elements, newValue);
+        }
+    });
+    const setValues = (elements, value) => Object.keys(elements).forEach(key => elements[key].value = value);
     attachEvent(bindClasses);
 })();
